3eddbc697709fb61944972d8b1a86456cbf6928d
Shusil Limbu Pangdhak <117982350+ShusilLimbu@users.noreply.github.com>
2026-04-29 16:24:26 +0545

    workflows file added

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,6 +8,7 @@ exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   const hashed = await bcrypt.hash(password, 10);
+  console.log("adkfj;;;;;;")
 
   const user = await User.create({
     name,
